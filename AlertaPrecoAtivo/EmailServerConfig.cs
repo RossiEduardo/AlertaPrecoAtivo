5cd8003ef9a675dd2798e68df2d1ca93d7690472
RossiEduardo <eduardorossi80@hotmail.com>
2023-04-07 17:24:25 -0300

    Sistema de email SMTP feito e modularizado

---
+++ AlertaPrecoAtivo/EmailServerConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailStuffs{
+    public class EmailServerConfig{
+        public string EmailSource { get; set; }
+        public string Name { get; set; }
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public Boolean SSL { get; set; }
+    }
+
+    public class Email{
+        public static void SendEmail(EmailServerConfig account, string dest_email, string subject, string body){
+            //email
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(account.EmailSource);
+            message.To.Add(new MailAddress(dest_email));
+            message.Subject = subject;
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+            message.Body = body;
+
+            //smtp
+            SmtpClient smtpClient = new SmtpClient(account.Server, account.Port);
+            smtpClient.EnableSsl = account.SSL;
+            smtpClient.Credentials = new NetworkCredential(account.EmailSource, account.Password);
+            smtpClient.Host = account.Server;
+
+            try
+            {
+                // realiza o envio da mensagem
+                smtpClient.Send(message);
+
+                Console.WriteLine("enviado");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro no envio de email!");
+            }
+        }
+    }
+}
+
--- AlertaPrecoAtivo/Program.cs
+++ AlertaPrecoAtivo/Program.cs
@@ -1,53 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Web.Script.Serialization;
 using RestSharp;
 using Newtonsoft.Json.Linq;
 using System.Globalization;
+using Microsoft.Extensions.Configuration;
+using EmailStuffs;
 
 
 namespace AlertaPrecoAtivo
 {
-    class Program{
-        static void Main(string[] args){
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("hello world");
-            string symbol = Console.ReadLine();
-            var stock = new AlphaVantageAPI(symbol, "ZQ46C6IRILKNDBH7");
-            Console.WriteLine(stock.getPrice());
+            //string symbol = Console.ReadLine();
+            //double gain = Convert.ToDouble(Console.ReadLine());
+            //double loss = Convert.ToDouble(Console.ReadLine());
+
+            //var stock = new AlphaVantageAPI(symbol, gain, loss, "ZQ46C6IRILKNDBH7");
+            //stock.GetPrice();
+
+            //email
+            //recuperando os valores do arquivo json
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddJsonFile("smtpconfig.json", false, true)
+                .AddEnvironmentVariables();
+            var config = builder.Build();
+            var account = config.GetSection("EmailServerConfig").Get<EmailServerConfig>();
+
+            Email.SendEmail(account, "eduardorossi80@hotmail.com", "Teste 2=3", "Sera q foi? testandoo");
+
+
         }
     }
 
-    public class AlphaVantageAPI{
+    public class AlphaVantageAPI {
         //propriedades
         public string _symbol;
         public string _myApiKey;
-        private float _price;
+        private double _price;
 
-        public AlphaVantageAPI(string symbol, string api_key){
+        public AlphaVantageAPI(string symbol, double gain, double loss, string api_key) {
             this._symbol = symbol;
             this._myApiKey = api_key;
             this._price = 0.00f;
         }
 
-        public float getPrice(){
+        public void GetPrice() {
+            double current_price = 0.00f;
+            Console.WriteLine("oi");
             var client = new RestClient("https://www.alphavantage.co");
             var request = new RestRequest($"query?function=GLOBAL_QUOTE&symbol={_symbol}&apikey={_myApiKey}", Method.Get);
             var response = client.Execute(request);
 
-            if (response.IsSuccessful){
+            if (response.IsSuccessful) {
                 dynamic data = JObject.Parse(response.Content);
                 string price = data["Global Quote"]["05. price"];
-                Console.WriteLine($"O preço da ação {_symbol} é {price}");
-                this._price = float.Parse(price, CultureInfo.InvariantCulture);
-                this._price = (float)Math.Round(this._price, 2);
+                this._price = Convert.ToDouble(price, CultureInfo.InvariantCulture);
 
+                //se for diferente atualiza o current_price
+                if (current_price != this._price) {
+                    current_price = this._price;
+                    Console.WriteLine(current_price);
+                }
             }
-            return this._price;
         }
-
     }
 }
